3fbab6b97aaa3f81c8ea2fe32c2b09b349d1fd59
nishantk045 <36162367+nishantk045@users.noreply.github.com>
2022-02-25 02:54:42 +0530

    Powershell script to dowload S3 bucket

---
+++ Download_S3_bucket_&_subfolders.ps1
@@ -0,0 +1,14 @@
+﻿$sourceBucket = 'mypayslip1892097'
+$profile = 'MyNewProfile'
+$Folder = 'F:\s3'
+ 
+$items = Get-S3Object -BucketName $sourceBucket -ProfileName $profile -Region 'us-east-1'
+Write-Host "$($items.Length) objects to copy"
+$index = 1
+$items | % {
+    Write-Host "$index/$($items.Length): $($_.Key)"
+    $fileName = $Folder + ".\$($_.Key.Replace('/','\'))"
+    Write-Host "$fileName"
+    Read-S3Object -BucketName $sourceBucket -Key $_.Key -File $fileName -ProfileName $profile -Region 'us-east-1' > $null
+    $index += 1
+}
